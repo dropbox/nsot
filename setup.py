782828e740542fafcbd2775e538fb935ca234b2e
jathanism <jathan@gmail.com>
2015-07-20 16:01:04 -0700

    Fix including of static files in setup.py install.
    - Also make sure that tests packages aren't included.

--- setup.py
+++ setup.py
@@ -20,6 +20,19 @@ with open('requirements.txt') as requirements:
     required = requirements.read().splitlines()
 
 
+package_data = {}
+def get_package_data(package, base_dir):
+    for dirpath, dirnames, filenames in os.walk(base_dir):
+        dirpath = dirpath[len(package)+1:]  # Strip package dir
+        for filename in filenames:
+            package_data.setdefault(package, []).append(os.path.join(dirpath, filename))
+        for dirname in dirnames:
+            get_package_data(package, dirname)
+
+get_package_data('nsot', 'nsot/static/build')
+get_package_data('nsot', 'nsot/templates')
+
+
 class PyTest(TestCommand):
     user_options = [('pytest-args=', 'a', 'Arguments to pass to py.test')]
 
@@ -75,7 +88,8 @@ class BuildStatic(Command):
 kwargs = {
     'name': 'nsot',
     'version': str(__version__),
-    'packages': find_packages(exclude=['tests']),
+    'packages': find_packages(exclude=['tests*']),
+    'package_data': package_data,
     'description': 'Network Source of Truth (IP Address Management).',
     'author': 'Gary M. Josack',
     'maintainer': 'Gary M. Josack',
