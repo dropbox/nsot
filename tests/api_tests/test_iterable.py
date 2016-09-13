5b6f672bf5d03650dcd95a9c160b8bbf79edf59d
Ajay Chenampara <ajay.chenampara@gmail.com>
2016-09-13 16:45:40 -0400

    Fixed API POST 400 error

--- tests/api_tests/test_iterable.py
+++ tests/api_tests/test_iterable.py
@@ -24,165 +24,155 @@ log = logging.getLogger(__name__)
 
 
 def test_creation(client, site):
-    """Test creation of Attributes."""
+    """Test creation of Iterables."""
 
-    attr_uri = site.list_uri('attribute')
+    itr_uri = site.list_uri('iterable')
+    # Successfully create an iterable
+    itr_resp = client.create(itr_uri, name='itr1', description="test-iterable", min_val=10, max_val=20, increment=1)
+    itr = get_result(itr_resp)
+    itr_obj_uri = site.detail_uri('iterable', id=itr['id'])
+    assert_created(itr_resp, itr_obj_uri)
 
-    # Test invalid attribute name
-    assert_error(
-        client.create(
-            attr_uri,
-            resource_name='Network', name='invalid attr1'
-        ),
-        status.HTTP_400_BAD_REQUEST
-    )
-
-    # Successfully create an Attribute
-    attr_resp = client.create(attr_uri, resource_name='Network', name='attr1')
-    attr = get_result(attr_resp)
-    attr_obj_uri = site.detail_uri('attribute', id=attr['id'])
-    assert_created(attr_resp, attr_obj_uri)
-
-    # Successfully get all Network Attributes
-    payload = get_result(attr_resp)
+    # Successfully get all s
+    payload = get_result(itr_resp)
     expected = [payload]
-    assert_success(client.get(attr_uri), expected)
-
+    assert_success(client.get(itr_uri), expected)
+#
     # Successfully get a single Network Attribute
-    assert_success(client.get(attr_obj_uri), payload)
+    #assert_success(client.get(attr_obj_uri), payload)
 
 
-def test_bulk_operations(client, site):
-    """Test creating/updating multiple Attributes at once."""
-
-    attr_uri = site.list_uri('attribute')
-
-    # Successfully create a collection of Attributes
-    collection = [
-        {'name': 'attr1', 'resource_name': 'Network'},
-        {'name': 'attr2', 'resource_name': 'Network'},
-        {'name': 'attr3', 'resource_name': 'Network'},
-    ]
-    collection_response = client.post(
-        attr_uri,
-        data=json.dumps(collection)
-    )
-    assert_created(collection_response, None)
-
-    # Successfully get all created Attributes
-    output = collection_response.json()
-    payload = get_result(output)
-
-    assert_success(
-        client.get(attr_uri),
-        payload
-    )
-
-    # Test bulk update to add a description to each Attribute
-    updated = copy.deepcopy(payload)
-
-    for item in updated:
-        item['description'] = 'This is the best attribute ever.'
-    updated_resp = client.put(attr_uri, data=json.dumps(updated))
-    expected = updated_resp.json()
-
-    assert updated == expected
-
-
-def test_update(client, site):
-    """Test updating Attributes w/ PUT."""
-
-    attr_uri = site.list_uri('attribute')
-
-    attr_resp = client.create(attr_uri, resource_name='Network', name='attr1')
-    attr = get_result(attr_resp)
-    attr_obj_uri = site.detail_uri('attribute', id=attr['id'])
-
-    # Update the description
-    params = {'description': 'Attribute 1'}
-    attr1 = copy.deepcopy(attr)
-    attr1.update(params)
-
-    assert_success(
-        client.update(attr_obj_uri, **params),
-        attr1
-    )
-
-    # Update the required flag; which should also set display=True
-    params = {'required': True}
-    attr2 = copy.deepcopy(attr1)
-    attr2.update(params)
-    attr2['display'] = True
-
-    assert_success(
-        client.update(attr_obj_uri, **params),
-        attr2
-    )
-
-    # Reset the object back to it's initial state!
-    assert_success(
-        client.update(attr_obj_uri, **attr),
-        attr
-    )
-
-
-def test_partial_update(site, client):
-    """Test PATCH operations to partially update an Attribute."""
-    attr_uri = site.list_uri('attribute')
-
-    attr_resp = client.create(attr_uri, resource_name='Network', name='attr1')
-    attr = get_result(attr_resp)
-    attr_pk_uri = site.detail_uri('attribute', id=attr['id'])
-
-    # Update display
-    params = {'display': True}
-    payload = copy.deepcopy(attr)
-    payload.update(params)
-
-    assert_success(
-        client.partial_update(attr_pk_uri, **params),
-        payload
-    )
-
-    # Update constraints
-    params = {
-        u'constraints': {
-            u'allow_empty': True, u'pattern': u'', u'valid_values': []
-        }
-    }
-    payload.update(params)
-    assert_success(
-        client.partial_update(attr_pk_uri, **params),
-        payload
-    )
-
-
-def test_deletion(client, site):
-    """Test DELETE operations for Attributes."""
-
-    attr_uri = site.list_uri('attribute')
-    net_uri = site.list_uri('network')
-
-    attr_resp = client.create(attr_uri, resource_name='Network', name='attr1')
-    attr = get_result(attr_resp)
-    attr_obj_uri = site.detail_uri('attribute', id=attr['id'])
-
-    # Create a Network with an attribute
-    net_resp = client.create(
-        net_uri, cidr='10.0.0.0/24', attributes={'attr1': 'foo'}
-    )
-    net = get_result(net_resp)
-
-    net_obj_uri = site.detail_uri('network', id=net['id'])
-
-    # Don't allow delete when there's an attached network
-    assert_error(
-        client.delete(attr_obj_uri),
-        status.HTTP_409_CONFLICT
-    )
-
-    # Now delete the Network
-    client.delete(net_obj_uri)
-
-    # And safely delete the Attribute
-    assert_deleted(client.delete(attr_obj_uri))
+#def test_bulk_operations(client, site):
+#    """Test creating/updating multiple Attributes at once."""
+#
+#    attr_uri = site.list_uri('attribute')
+#
+#    # Successfully create a collection of Attributes
+#    collection = [
+#        {'name': 'attr1', 'resource_name': 'Network'},
+#        {'name': 'attr2', 'resource_name': 'Network'},
+#        {'name': 'attr3', 'resource_name': 'Network'},
+#    ]
+#    collection_response = client.post(
+#        attr_uri,
+#        data=json.dumps(collection)
+#    )
+#    assert_created(collection_response, None)
+#
+#    # Successfully get all created Attributes
+#    output = collection_response.json()
+#    payload = get_result(output)
+#
+#    assert_success(
+#        client.get(attr_uri),
+#        payload
+#    )
+#
+#    # Test bulk update to add a description to each Attribute
+#    updated = copy.deepcopy(payload)
+#
+#    for item in updated:
+#        item['description'] = 'This is the best attribute ever.'
+#    updated_resp = client.put(attr_uri, data=json.dumps(updated))
+#    expected = updated_resp.json()
+#
+#    assert updated == expected
+#
+#
+#def test_update(client, site):
+#    """Test updating Attributes w/ PUT."""
+#
+#    attr_uri = site.list_uri('attribute')
+#
+#    attr_resp = client.create(attr_uri, resource_name='Network', name='attr1')
+#    attr = get_result(attr_resp)
+#    attr_obj_uri = site.detail_uri('attribute', id=attr['id'])
+#
+#    # Update the description
+#    params = {'description': 'Attribute 1'}
+#    attr1 = copy.deepcopy(attr)
+#    attr1.update(params)
+#
+#    assert_success(
+#        client.update(attr_obj_uri, **params),
+#        attr1
+#    )
+#
+#    # Update the required flag; which should also set display=True
+#    params = {'required': True}
+#    attr2 = copy.deepcopy(attr1)
+#    attr2.update(params)
+#    attr2['display'] = True
+#
+#    assert_success(
+#        client.update(attr_obj_uri, **params),
+#        attr2
+#    )
+#
+#    # Reset the object back to it's initial state!
+#    assert_success(
+#        client.update(attr_obj_uri, **attr),
+#        attr
+#    )
+#
+#
+#def test_partial_update(site, client):
+#    """Test PATCH operations to partially update an Attribute."""
+#    attr_uri = site.list_uri('attribute')
+#
+#    attr_resp = client.create(attr_uri, resource_name='Network', name='attr1')
+#    attr = get_result(attr_resp)
+#    attr_pk_uri = site.detail_uri('attribute', id=attr['id'])
+#
+#    # Update display
+#    params = {'display': True}
+#    payload = copy.deepcopy(attr)
+#    payload.update(params)
+#
+#    assert_success(
+#        client.partial_update(attr_pk_uri, **params),
+#        payload
+#    )
+#
+#    # Update constraints
+#    params = {
+#        u'constraints': {
+#            u'allow_empty': True, u'pattern': u'', u'valid_values': []
+#        }
+#    }
+#    payload.update(params)
+#    assert_success(
+#        client.partial_update(attr_pk_uri, **params),
+#        payload
+#    )
+#
+#
+#def test_deletion(client, site):
+#    """Test DELETE operations for Attributes."""
+#
+#    attr_uri = site.list_uri('attribute')
+#    net_uri = site.list_uri('network')
+#
+#    attr_resp = client.create(attr_uri, resource_name='Network', name='attr1')
+#    attr = get_result(attr_resp)
+#    attr_obj_uri = site.detail_uri('attribute', id=attr['id'])
+#
+#    # Create a Network with an attribute
+#    net_resp = client.create(
+#        net_uri, cidr='10.0.0.0/24', attributes={'attr1': 'foo'}
+#    )
+#    net = get_result(net_resp)
+#
+#    net_obj_uri = site.detail_uri('network', id=net['id'])
+#
+#    # Don't allow delete when there's an attached network
+#    assert_error(
+#        client.delete(attr_obj_uri),
+#        status.HTTP_409_CONFLICT
+#    )
+#
+#    # Now delete the Network
+#    client.delete(net_obj_uri)
+#
+#    # And safely delete the Attribute
+#    assert_deleted(client.delete(attr_obj_uri))
