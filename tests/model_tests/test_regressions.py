dropbox/nsot

commit 57c04d0da113cb10e551d326237b2a897f2d850b

Improve performance in Network.get_next_network() for large prefixes (#226)
- The fix in #224 introduced a notable performance bug due to iterating
  all descendents vs. only direct children.
- This patch addresses the performance issue by attempting to pre-seed
  the list of dirty networks via excluding ones with ineligible prefix
  lengths as well as immediately checking whether a candidate subnet is
  dirty BEFORE iterating child networks vs. AFTER.

nsot/models.py
@@ -15,6 +15,7 @@ import logging
 import netaddr
 from operator import attrgetter
 import re
+import time
 
 from . import exc
 from . import fields
@@ -793,6 +794,8 @@ class Network(Resource):
         :returns:
             list(IPNetwork)
         """
+        start_time = time.time()  # For debugging
+
         # If we're reserved, automatically ZILCH!!
         # TODO(jathan): Should we raise an error instead?
         if self.state == Network.RESERVED:
@@ -833,13 +836,29 @@ class Network(Resource):
         # Exclude children that are in busy states.
         children = self.get_descendents()
 
-        # FIXME(jathan): This can potentially be very slow if the gap between
-        # parent and child networks is large, say, on the order of /8.
-        # Something to keep in mind if it comes up in practical application,
-        # especially with IPv6 networks, which is still not heavily tested.
-        wanted = []  # Subnets we want.
-        dirty = []   # Dirty subnets (have children)
-        children_seen = []  # For child networks we've already processed.
+        # Pre-filter children and store the `.ip_network` property up front.
+        children = [
+            c.ip_network for c in children if (
+                c.state not in self.BUSY_STATES
+            )
+        ]
+
+        # Prepopulate children_seen with networks that have the same prefix
+        # length as wanted `prefix_length`.
+        children_seen = [
+            c for c in children if (
+                c.prefixlen == prefix_length
+            )
+        ]
+
+        # Dirty subnets cannot be used. Pre-seed w/ children_seen.
+        dirty = children_seen[:]
+
+        # Pre-filter any pre-seeded dirty subnets as a generator.
+        subnets = (s for s in subnets if s not in dirty)
+
+        # Subnets we want.
+        wanted = []
 
         # Keep iterating until we've found the number of networks we want.
         while len(wanted) < num:
@@ -850,14 +869,15 @@ class Network(Resource):
             except StopIteration:
                 break
 
-            # We can't allocate ourself.
-            if next_subnet == cidr:
+            log.debug('>> CHILDREN_SEEN: %s items, DIRTY: %s items',
+                      len(children_seen), len(dirty))
+
+            # Skip dirty subnets immediately.
+            if next_subnet in dirty:
                 continue
 
             # Never return 1st/last addresses if prefix is for an address,
             # unless it's an interconnect (aka point-to-point).
-            # FIXME(jathan): Revisit why we made this decision. It seems
-            # arbitrary.
             if cidr.prefixlen in settings.NETWORK_INTERCONNECT_PREFIXES:
                 pass
             elif (next_subnet.prefixlen in settings.HOST_PREFIXES and
@@ -870,39 +890,17 @@ class Network(Resource):
 
             # Iterate the children and if we make it to the end, we've found a
             # keeper!
-            for child in children:
-                # This child is busy; mark it as seen.
-                if child.state in self.BUSY_STATES:
-                    children_seen.append(child.ip_network)
-                    continue
-
-                # Network is already wanted; skip it!
-                if next_subnet in wanted:
-                    log.debug('Network %s already wanted; skipping',
-                              next_subnet)
-                    break
-
-                # This child has already been seen; skip it!
-                if child.ip_network in children_seen:
-                    continue
+            unseen_children = set(children).difference(children_seen)
+            log.debug('>> UNSEEN_CHILDREN: %s items', len(unseen_children))
+            for child in unseen_children:
 
                 # This network is already in use/allocated; skip it!
-                # TODO(jathan): Decide if we want 'allocated' to also be a busy
-                # state for the purpose of allocation? Yes, that's confusing,
-                # but why not just treat this as a busy state? What about
-                # orphaned state?
-                if child.ip_network.overlaps(next_subnet):
-                    log.debug('Child %s network overlaps w/ next_subnet %s',
-                              child.ip_network, next_subnet)
-                    children_seen.append(child.ip_network)
+                if child.overlaps(next_subnet):
+                    children_seen.append(child)
                     continue
 
             # We *might* want this subnet. But make sure it's clean first.
             else:
-                # Skip dirty subnets.
-                if next_subnet in dirty:
-                    continue
-
                 # Check all the children we've seen; if next_subnet contains
                 # it, mark it as dirty.
                 for child in children_seen:
@@ -925,6 +923,8 @@ class Network(Resource):
 
         log.info('>> WANTED = %s', wanted)
 
+        elapsed_time = time.time() - start_time
+        log.debug('>> ELAPSED TIME: %s' % elapsed_time)
         return wanted if as_objects else [unicode(w) for w in wanted]
 
     def get_next_address(self, num=None, as_objects=True):

docker/Dockerfile
@@ -37,7 +37,7 @@ RUN pip install psycopg2
 
 # Try to run this as late as possible for layer caching - this version will be
 # updated every update so let the build not take longer than necessary
-RUN pip install nsot==1.0.5
+RUN pip install nsot==1.0.6
 COPY conf /etc/nsot
 
 ENTRYPOINT ["nsot-server", "--config=/etc/nsot/nsot.conf.py"]

nsot/version.py
@@ -1,1 +1,1 @@
-__version__ = '1.0.5'
+__version__ = '1.0.6'

commit ed9fec2eb4087e165a523c82441ddca8e7d348cf

Fix for get_next_network assigned state regression
A regression was introduced in #226 to where if a child was in the
assigned state (such as a /32 assigned to an interface), it would be
considered as a candidate for get_next_network, while allocated children
where not.

My fix for this builds the list of dirty subnets as the set of Networks
that match the prefix length we're looking for and are in a 'busy' state,
which are the subnets that should be immediately discounted as we look
through all the possible subnets of the desired prefix length.

I've also added a test to the regression tests to check for this. I've
verified that the test fails with the version of models.py that excludes
my fix.

nsot/models.py
@@ -858,7 +858,13 @@ class Network(Resource):
         # Exclude children that are in busy states.
         children = self.get_descendents()
 
-        # Pre-filter children and store the `.ip_network` property up front.
+        # Partition children into busy children and non-busy children, storing
+        # the `.ip_network` property up front.
+        busy_children = [
+            c.ip_network for c in children if (
+                c.state in self.BUSY_STATES
+            )
+        ]
         children = [
             c.ip_network for c in children if (
                 c.state not in self.BUSY_STATES
@@ -873,8 +879,8 @@ class Network(Resource):
             )
         ]
 
-        # Dirty subnets cannot be used. Pre-seed w/ children_seen.
-        dirty = children_seen[:]
+        # Dirty subnets cannot be used. Pre-seed w/ busy children.
+        dirty = [c for c in busy_children if (c.prefixlen == prefix_length)]
 
         # Pre-filter any pre-seeded dirty subnets as a generator.
         subnets = (s for s in subnets if s not in dirty)

tests/model_tests/test_regressions.py
@@ -84,3 +84,35 @@ def test_next_network_bug_issues_224(site):
     next_31 = parent.get_next_network(prefix_length=31)
 
     assert next_31 == expected
+
+
+def test_next_network_bug_issues_247(site):
+    """
+    Test for a bug where Network.get_next-network() returns an address that is
+    in the assigned state, but not any that are in the allocated state
+
+    Ref: https://github.com/dropbox/nsot/issues/247
+    """
+
+    objects = load_json('model_tests/data/networks.json')
+    [models.Network.objects.create(site=site, **n) for n in objects]
+
+    parent = models.Network.objects.get_by_address('10.20.0.0/16')
+
+    # Create a couple of /32s to push our expected /32 up
+    models.Network.objects.create(
+        site=site,
+        cidr='10.20.0.1/32',
+        state=models.Network.ASSIGNED
+    )
+    models.Network.objects.create(site=site, cidr='10.20.0.2/32')
+
+    expected_32 = [ipaddress.ip_network('10.20.0.3/32')]
+    next_32 = parent.get_next_network(prefix_length=32)
+
+    assert next_32 == expected_32
+
+    expected_24 = [ipaddress.ip_network('10.20.30.0/24')]
+    next_24 = parent.get_next_network(prefix_length=24)
+
+    assert next_24 == expected_24