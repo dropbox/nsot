68335f13923405046ed94bc83c536114c2fb0dc3
ajay.chenampara <ajay.chenampara@gmail.com>
2017-02-01 05:22:41 -0500

    merge migrations

---
+++ nsot/migrations/0032_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('nsot', '0031_populate_circuit_name_slug'),
+        ('nsot', '0030_auto_20161006_0416'),
+    ]
+
+    operations = [
+    ]
