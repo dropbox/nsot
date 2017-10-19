1d5051ece48091cb90ac6699a5daa82498816fcd
Nick Pegg <npegg@dropbox.com>
2017-10-19 21:14:07 +0000

    Re-work the Protocol model
    The `Protocol.type` field has been changed from a string to a
    relation to a new `ProtocolType` model. This new model allows you to
    define your own types such as BGP, OSPF, etc. and define Protocol
    attributes which are required. Further documentation on how to exactly
    use this will be included with the PR that adds the views that work with
    Protocol objects.
    
    Along with that flexibility, some of the concrete fields have been
    removed from Protocol, like `asn` which was a bit of an overloaded
    number value. Instead, it's expected that fields like this would be
    attributes and any attributes required by a specific protocol type be
    managed via the `ProtocolType` model.
    
    Additionally an `interface` field was added to `Protocol` to better
    facilitate routing protocols like OSPF and IS-IS which are configured by
    their interface. Before it was expected that the `circuit` would contain
    this information, which was cumbersome. Neither `interface` nor
    `circuit` are required fields to keep things flexible for folks.
    
    Thanks to @jathanism for the `ProtocolType` idea and some of the code
    around it.

--- tests/model_tests/test_protocols.py
+++ tests/model_tests/test_protocols.py
@@ -10,12 +10,56 @@ from .fixtures import circuit, site
 pytestmark = pytest.mark.django_db
 
 
+@pytest.fixture
+def asn_attribute(site):
+    return models.Attribute.objects.create(
+        site=site,
+        name='asn',
+        resource_name='Protocol',
+    )
+
+
+@pytest.fixture
+def bgp(asn_attribute):
+    bgp = models.ProtocolType.objects.create(
+        name='bgp',
+        description='Border Gateway Protocol',
+    )
+    bgp.required_attributes.add(asn_attribute)
+
+    return bgp
+
+
+@pytest.fixture
+def area_attribute(site):
+    return models.Attribute.objects.create(
+        site=site,
+        name='area',
+        resource_name='Protocol',
+    )
+
+
+@pytest.fixture
+def ospf(area_attribute):
+    ospf = models.ProtocolType.objects.create(
+        name='ospf',
+        description='Open Shortest Path First protocol',
+    )
+    ospf.required_attributes.add(area_attribute)
+
+    return ospf
+
+
 class TestCreation(object):
     @pytest.fixture
     def device(self, circuit):
         return circuit.endpoint_a.device
 
-    def test_normal_conditions(self, device, circuit):
+    @pytest.fixture
+    def interface(self, circuit):
+        return circuit.endpoint_a
+
+    def test_normal_conditions(self, device, circuit, bgp):
         """
         Under normal conditions, ensure that a Protocol can be created without
         raising any exceptions
@@ -23,16 +67,19 @@ class TestCreation(object):
         protocol = models.Protocol.objects.create(
             device=device,
             circuit=circuit,
-            type='bgp',
-            asn=1234,
-            description='My fancy peer'
+            type=bgp,
+            description='My fancy peer',
+            attributes={
+                'asn': '1234',
+            }
         )
+        protocol.save()
 
         assert protocol.site_id == device.site_id
         assert protocol in device.protocols.all()
         assert protocol in circuit.protocols.all()
 
-    def test_bad_circuit(self, device, circuit):
+    def test_bad_circuit(self, device, circuit, bgp):
         """
         Ensure a ValidationError is thrown when we try to create a Protocol
         where the Circuit is not attached to the Device
@@ -44,56 +91,112 @@ class TestCreation(object):
             models.Protocol.objects.create(
                 device=device,
                 circuit=bad_circuit,
-                type='bgp',
-                asn=1234
+                type=bgp,
+                attributes={
+                    'asn': '1234',
+                }
             )
 
-    def test_bad_type(self, device, circuit):
-        with pytest.raises(exc.ValidationError):
-            models.Protocol.objects.create(
-                device=device,
-                circuit=circuit,
-                type='oh no',
-                asn=1234
-            )
-
-    def test_attributes(self, device, circuit):
-        """ Ensure that we can set attributes on a Protocol """
+    def test_attributes(self, device, circuit, bgp):
+        """ Ensure that we can set arbitrary attributes on a Protocol """
         models.Attribute.objects.create(
             site=device.site, resource_name='Protocol', name='import_policies')
 
         protocol = models.Protocol.objects.create(
             device=device,
             circuit=circuit,
-            type='bgp',
-            asn=1234,
+            type=bgp,
             attributes={
+                'asn': '1234',
                 'import_policies': 'foo'
             }
         )
 
         assert protocol.get_attributes()['import_policies'] == 'foo'
 
+    def test_missing_attributes(self, device, circuit, bgp):
+        """
+        Test that a ValidationError is thrown when we try to create a Protocol.
+        The BGP ProtocolType we defined has a required attribute `asn`
+        """
+        with pytest.raises(exc.ValidationError):
+            models.Protocol.objects.create(
+                device=device,
+                circuit=circuit,
+                type=bgp,
+                attributes={},
+            )
+
+    def test_another_protocol(self, device, interface, ospf):
+        """
+        Test that we can add a Protocol with second ProtocolType and that it
+        doesn't raise any sort of exception
+        """
+        models.Protocol.objects.create(
+            device=device,
+            interface=interface,
+            type=ospf,
+            attributes={'area': 'threeve'}
+        )
+
+    def test_mixed_attrs(self, device, circuit, bgp, area_attribute):
+        """
+        Test that we can add a Protocol of one ProtocolType with an attribute
+        that's required by another ProtocolType without anything weird
+        happening
+        """
+        protocol = models.Protocol.objects.create(
+            device=device,
+            circuit=circuit,
+            type=bgp,
+            attributes={
+                'asn': '1234',
+                'area': 'threeve',
+            }
+        )
+
+        assert protocol.get_attributes()['asn'] == '1234'
+        assert protocol.get_attributes()['area'] == 'threeve'
+
 
 class TestInterfaces(object):
     @pytest.fixture
-    def protocol(self, circuit):
+    def protocol(self, circuit, bgp):
         device = circuit.endpoint_a.device
 
         return models.Protocol.objects.create(
             device=device,
             circuit=circuit,
-            type='bgp',
-            asn=1234
+            type=bgp,
+            attributes={
+                'asn': '1234',
+            }
         )
 
-    def test_local_interface(self, circuit, protocol):
+    @pytest.fixture
+    def interface(self, circuit):
+        return circuit.endpoint_a
+
+    @pytest.fixture
+    def interface_protocol(self, interface, ospf):
+        device = interface.device
+
+        return models.Protocol.objects.create(
+            device=device,
+            interface=interface,
+            type=ospf,
+            attributes={'area': '1234'},
+        )
+
+    def test_local_interface(self, circuit, protocol, interface,
+                             interface_protocol):
         assert protocol.local_interface() == circuit.endpoint_a
+        assert interface_protocol.local_interface() == interface
 
     def test_remote_interface(self, circuit, protocol):
         assert protocol.remote_interface() == circuit.endpoint_z
 
-    def test_backwards_circuit(self, circuit):
+    def test_backwards_circuit(self, circuit, bgp):
         """
         Make sure local/remote_interface returns the correct thing when the
         device is on the Z side of the circuit instead of the A side
@@ -101,8 +204,10 @@ class TestInterfaces(object):
         protocol = models.Protocol.objects.create(
             device=circuit.endpoint_z.device,
             circuit=circuit,
-            type='bgp',
-            asn=1234
+            type=bgp,
+            attributes={
+                'asn': '1234',
+            }
         )
 
         assert protocol.local_interface() == circuit.endpoint_z
